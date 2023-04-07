0b81c23a205d9a4ada9be9356a39ff7fc9749f75
wickedev <orange881217@gmail.com>
2023-04-07 09:11:54 +0900

    remove base

--- pull-altair-static.sh
+++ pull-altair-static.sh
@@ -6,8 +6,12 @@ rm -rf resources/altair
 mv package/build/dist resources/altair
 rm -rf package
 
+body_closing_pattern="<\/body>"
+altair_init_replacement="  <script>AltairGraphQL.init({{config-options|json|safe}})<\/script>\n<\/body>"
 
-pattern="<\/body>"
-replacement="  <script>AltairGraphQL.init({{config-options|json|safe}})<\/script>\n<\/body>"
+sed -i '' -E "s/$body_closing_pattern/$altair_init_replacement/g" resources/altair/index.html
 
-sed -i '' -E "s/$pattern/$replacement/g" resources/altair/index.html
+base_pattern="    <base href=\"\/\" \/>"
+base_remove_replacement=""
+
+sed -i '' -E "s/$base_pattern/$base_remove_replacement/g" resources/altair/index.html
